3c14c1f70bff02c0975c81942f7dbbe704dc5e4c
ZioSamuele <ziosamuele@mail.com>
2013-07-22 15:14:15 +0200

    Commented SmartPointer1.h

--- SmartPointer1.h
+++ SmartPointer1.h
@@ -1,25 +1,78 @@
 #ifndef SMARTPOINTER1_H
 #define SMARTPOINTER1_H
 
+/*!
+ * SmartPointer1
+ * \brief A smart pointer based on a reference counter.
+ */
+
 template <typename T>
 class SmartPointer1
 {
     public:
+        /*!
+         * Consructor
+         * \brief Initialize a new smart pointer
+         * \param ptr An existing pointer to a USER ALLOCATED memory
+         */
         SmartPointer1(T*);
+
+        /*!
+         * Destructor
+         * \brief Delete reference to the pointer and delete it if no one left
+         */
         virtual ~SmartPointer1();
-        SmartPointer1(const SmartPointer1<T>&);
+
+        /*!
+         * Copy constructor
+         * \brief Initialize from an existing smart pointer
+         * \param smartptr An existing smart pointer
+         */
+        SmartPointer1(const SmartPointer1<T>& smartptr);
+
+        /*!
+         * Structure dereference operator
+         * \return The wrapped pointer itself
+         */
         T* operator->();
+
+        /*!
+         * Indirection operator
+         * \return A reference to the object pointed by the wrapped pointer
+         */
         T& operator* ();
-        SmartPointer1<T>& operator=(const SmartPointer1<T>&);
+
+        /*!
+         * Assignement operator
+         * \param smartptr An existing SmartPointer instance
+         * \return A freshly initialized SmartPointer
+         */
+        SmartPointer1<T>& operator=(const SmartPointer1<T>& smartptr);
     private:
+        /*!
+         * Reference counting object
+         * \brief Store the wrapped pointer and its reference counter
+         */
         struct Counter{
+            /*!
+             * Constructor
+             * \brief Initialize the reference counter
+             * \param ptr A pointer to a USER ALLOCATED memory of type T
+             * \param c The reference count
+             */
             Counter(T* ptr, unsigned int c): p(ptr), count(c){}
             T *p;
             unsigned int count;
         }*counter;
 
+        /*!
+         * \brief Add our reference to the pointer
+         */
         void acquire(Counter*);
 
+        /*!
+         * \brief Decrement our reference to the pointer and delete it if no reference left
+         */
         void release();
 };
 
